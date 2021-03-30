157f0ffcd95981094aefea4cd1317fb45c485ded
spacexesau <81427878+spacexesau@users.noreply.github.com>
2021-03-29 21:54:37 -0400

    Sample Docstring
    Wrote a simple docstring that briefly explains our project. We can further elaborate on it.

--- Exercise.py
+++ Exercise.py
@@ -1,7 +1,36 @@
 print("Hello") # Nick
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("INST-326 Final Project") #Nelson C.
+
+"""
+The project idea is to come up with a dungeon/maze game 
+that pretty much consists of a text base system. Nicholas Koy
+came up with the idea and we all agreed on the project and it typically 
+runs by parsing a file. There will be barriers like walls and creatures 
+along the text based maze. 
+
+Further explanation (describe further):
+methods (mentioning methods used): 
+"""
+=======
+=======
+>>>>>>> d1051c6f1fe8d34a0951bd30ee82124de46f91aa
+>>>>>>> Stashed changes
 class Greet():
     """Greets the user."""
     def __init__(self,language):
         self.language = language
     
+<<<<<<< Updated upstream
     
+=======
+<<<<<<< HEAD
+    
+>>>>>>> d1051c6f1fe8d34a0951bd30ee82124de46f91aa
+=======
+    
+>>>>>>> d1051c6f1fe8d34a0951bd30ee82124de46f91aa
+>>>>>>> Stashed changes
