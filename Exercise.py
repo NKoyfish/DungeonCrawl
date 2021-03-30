5c4fda095380838a5758e1c7c27feb02fbf4c9ff
aiqbal24 <81580415+aiqbal24@users.noreply.github.com>
2021-03-30 18:21:22 -0400

    Update Exercise.py
    These are the potential methods we would be using for the maze game.

--- Exercise.py
+++ Exercise.py
@@ -2,6 +2,7 @@ print("Hello") # Nick
 
 print("INST-326 Final Project") #Nelson C.
 
+
 """
 The project idea is to come up with a dungeon/maze game 
 that pretty much consists of a text base system. Nicholas Koy
@@ -9,8 +10,26 @@ came up with the idea and we all agreed on the project and it typically
 runs by parsing a file. There will be barriers like walls and creatures 
 along the text based maze. 
 
-Further explanation (describe further):
-methods (mentioning methods used): 
+"""
+
+"""
+Potential Methods:
+
+battle(): similar to aardvark battle system.
+
+getScore(): calculated from hunger value and time it took to reach the end and treasure obtained.
+
+breakWall(): player skill limited use that breaks one wall that isn’t essential. (Border)
+
+jumpWall(): another skill could be used in exchange for hunger. Jumps wall if there is an empty space.
+
+revealMap(): Applied if a map is in player inventory.
+
+eatFood(): So that player can rejuvenate.
+
+rest(): Restore health and consume food. Lowers score
+
+run(): Runs from a battle
 """
 
 class Greet():
