a5738290afff04caf6ca6619dc041d4bc860eb59
aiqbal24 <81580415+aiqbal24@users.noreply.github.com>
2021-05-18 20:51:09 -0400

    Adding assert statements
    These assert statements will test players score based on the inventory.

--- ali_test.py
+++ ali_test.py
@@ -13,24 +13,24 @@ def test_getscore():
     score = gscore.getScore()
     player = dg.Player(name,hp,attack,speed,hunger)
     if i == "Diamond":
-        score += player.inventory[i]*100
+        assert score += player.inventory[i]*100
     elif(i == "Gold"):
-        score += player.inventory[i]*80
+        assert score += player.inventory[i]*80
     elif(i == "Emerald"):
-        score += player.inventory[i]*60
+        assert score += player.inventory[i]*60
     elif(i == "Silver"):
-        score += player.inventory[i]*50
+        assert score += player.inventory[i]*50
     elif(i == "Bronze"):
-        score += player.inventory[i]*35
+        assert score += player.inventory[i]*35
     elif(i == "Copper"):
-        score += player.inventory[i]*20
+        assert score += player.inventory[i]*20
     elif(i == "Amber"):
-        score += player.inventory[i]*15
+        assert score += player.inventory[i]*15
     elif(i == "Nugget"):
-        score += player.inventory[i]*10
+        assert score += player.inventory[i]*10
     elif i == "small core":
-        score += (75 * player.inventory[i])
+        assert score += (75 * player.inventory[i])
     elif i == "medium core":
-        score += (125 * player.inventory[i])
+        assert score += (125 * player.inventory[i])
     elif i == "large core":
-        score += (200 * player.inventory[i])
+        assert score += (200 * player.inventory[i])
