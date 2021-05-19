8e895366f3b2b16e415134159cac31f23ba08015
aiqbal24 <81580415+aiqbal24@users.noreply.github.com>
2021-05-18 20:38:46 -0400

    Using same conditional to test score based on inventory

--- ali_test.py
+++ ali_test.py
@@ -7,3 +7,28 @@ import builtins
 from unittest import mock
 from unittest.mock import patch
 from time import sleep
+
+
+def test_getscore():
+    if i == "Diamond":
+        score += self.inventory[i]*100
+    elif(i == "Gold"):
+        score += self.inventory[i]*80
+    elif(i == "Emerald"):
+        score += self.inventory[i]*60
+    elif(i == "Silver"):
+        score += self.inventory[i]*50
+    elif(i == "Bronze"):
+        score += self.inventory[i]*35
+    elif(i == "Copper"):
+        score += self.inventory[i]*20
+    elif(i == "Amber"):
+        score += self.inventory[i]*15
+    elif(i == "Nugget"):
+        score += self.inventory[i]*10
+    elif i == "small core":
+        score += (75 * self.inventory[i])
+    elif i == "medium core":
+        score += (125 * self.inventory[i])
+    elif i == "large core":
+        score += (200 * self.inventory[i])
