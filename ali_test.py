321f6b3b43a62263118167571909e62ca2c55d98
aiqbal24 <81580415+aiqbal24@users.noreply.github.com>
2021-05-18 21:04:19 -0400

    Finished my getscore() pytest

--- ali_test.py
+++ ali_test.py
@@ -10,27 +10,31 @@ from time import sleep
 
 
 def test_getscore():
-    score = gscore.getScore()
+    #score = gscore.getScore()
+    name,hp,attack,speed,hunger = "Ali",25,80,45,12
     player = dg.Player(name,hp,attack,speed,hunger)
-    if i == "Diamond":
-        assert score += player.inventory[i]*100
-    elif(i == "Gold"):
-        assert score += player.inventory[i]*80
-    elif(i == "Emerald"):
-        assert score += player.inventory[i]*60
-    elif(i == "Silver"):
-        assert score += player.inventory[i]*50
-    elif(i == "Bronze"):
-        assert score += player.inventory[i]*35
-    elif(i == "Copper"):
-        assert score += player.inventory[i]*20
-    elif(i == "Amber"):
-        assert score += player.inventory[i]*15
-    elif(i == "Nugget"):
-        assert score += player.inventory[i]*10
-    elif i == "small core":
-        assert score += (75 * player.inventory[i])
-    elif i == "medium core":
-        assert score += (125 * player.inventory[i])
-    elif i == "large core":
-        assert score += (200 * player.inventory[i])
+    score = 0
+    for i in player.inventory.keys():
+        if i == "Diamond":
+            assert score == score + player.inventory[i]*100
+        elif(i == "Gold"):
+            assert score == score + player.inventory[i]*80
+        elif(i == "Emerald"):
+            assert score == score + player.inventory[i]*60
+        elif(i == "Silver"):
+            assert score == score + player.inventory[i]*50
+        elif(i == "Bronze"):
+            assert score == score + player.inventory[i]*35
+        elif(i == "Copper"):
+            assert score == score + player.inventory[i]*20
+        elif(i == "Amber"):
+            assert score == score + player.inventory[i]*15
+        elif(i == "Nugget"):
+            assert score == score + player.inventory[i]*10
+        elif i == "small core":
+            assert score == score + (75 * player.inventory[i])
+        elif i == "medium core":
+            assert score == score + (125 * player.inventory[i])
+        elif i == "large core":
+            assert score == score + (200 * player.inventory[i])
+            
